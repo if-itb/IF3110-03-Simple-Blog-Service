662224240ddb43482ae4cc502b64aa272f1bbbf9
willy2706 <willylie2706@gmail.com>
2014-12-12 14:09:21 +0700

    setting github

---
+++ Server/src/Main.java
@@ -0,0 +1,20 @@
+
+import com.firebase.client.Firebase;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author WILLY
+ */
+public class Main {
+	public static void main(String[] args) {
+		Database db = Database.getDatabase();
+		Firebase fb = new Firebase("ok");
+	}
+//	db.setValue("Alex Wolfe");
+}
---
+++ Server/src/User.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author christangga
+ */
+public class User {
+
+	private int id;
+	private String email;
+	private String username;
+	private String password;
+	private String role;
+	
+	/**
+	 * Creates a new instance of User
+	 */
+	public User() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	@Override
+	public String toString() {
+		return String.valueOf(id) + " " + email + " " + username + " " + password + " " + role;
+	}
+}
