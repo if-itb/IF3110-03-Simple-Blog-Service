7cf7282934dd74ec0dea9a065385e0542da0f2aa
Kevin <kevin.zhong94@gmail.com>
2014-12-14 17:13:49 +0700

    tambah search

---
+++ local/src/controller/Search.java
@@ -0,0 +1,48 @@
+package controller;
+
+import java.rmi.RemoteException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+
+import org.wbd.heroku.service.FirebaseService;
+import org.wbd.heroku.service.FirebaseServiceProxy;
+
+import entities.Post;
+
+@ManagedBean
+@RequestScoped
+public class Search {
+	
+	public String keywords;
+	
+	public String getKeywords(){
+		return keywords;
+	}
+	
+	public void setKeywords(String katakunci){
+		this.keywords=katakunci;
+	}
+	
+	void searchwithFirebase(){		
+		FirebaseServiceProxy FBServiceProxy=new FirebaseServiceProxy();
+		try {
+			List<org.wbd.heroku.service.Post> FindedPost;
+			FindedPost = new ArrayList<org.wbd.heroku.service.Post>(Arrays.asList(FBServiceProxy.search(this.keywords)));
+		} catch (RemoteException e) {	
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
+}
