69d6846aa4f6be288e3fe7092fd77bbc0cd62cbb
Kevin <kevin.zhong94@gmail.com>
2014-12-15 21:08:58 +0700

    saatny bljr MBD

--- local/src/controller/Search.java
+++ local/src/controller/Search.java
@@ -1,13 +1,10 @@
 package controller;
 
 import java.rmi.RemoteException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -23,27 +20,61 @@ import entities.Post;
 @ManagedBean
 @RequestScoped
 public class Search {
-	
+
 	public String keywords;
-	
-	public String getKeywords(){
+	public String finalkeywords;
+
+	public String getKeywords() {
 		return keywords;
 	}
-	
-	public void setKeywords(String katakunci){
-		this.keywords=katakunci;
+
+	public void setKeywords(String katakunci) {
+		this.keywords = katakunci;
+	}
+
+	public void find(){
+		finalkeywords=keywords;
 	}
 	
-	void searchwithFirebase(){		
-		FirebaseService FBServiceProxy=new FirebaseServiceProxy();
-		try {
+	public List<Post> getwantedPostList() {
+		FirebaseService aeroga = new FirebaseServiceProxy();
+		List<Post> FindedPost = new ArrayList<>();
+		org.wbd.heroku.service.Post[] posts;
+		
+		//versi kmrn
+		/*try {
 			List<org.wbd.heroku.service.Post> FindedPost;
-			
-			FindedPost = new ArrayList<org.wbd.heroku.service.Post>(Arrays.asList(FBServiceProxy.search(this.keywords)));
-		} catch (RemoteException e) {	
+
+			FindedPost = new ArrayList<org.wbd.heroku.service.Post>(
+					Arrays.asList(FBServiceProxy.search(this.keywords)));
+		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
+		}*/
+		
+		try{
+			posts=aeroga.search(this.finalkeywords);
+			//System.out.println(this.finalkeywords);
+			//System.out.println(this.keywords);
+			
+			for (org.wbd.heroku.service.Post p : posts) {
+				Post post = new Post();
+				post.setId(p.getId());
+				post.setTitle(p.getJudul());
+				DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+				post.setDate(df.parse(p.getTanggal()));
+				post.setContent(p.getKonten());				
+				FindedPost.add(post);
+			}
+		}
+		catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return FindedPost;
+		
 	}
-	
 }
