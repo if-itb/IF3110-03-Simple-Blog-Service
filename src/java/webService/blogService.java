183fc7edc0f5f8404526969c52533b5dbc41bb37
akhfa <akhmadfakhoni@gmail.com>
2014-12-17 06:09:36 +0700

    Push new user

--- src/java/webService/blogService.java
+++ src/java/webService/blogService.java
@@ -5,9 +5,12 @@
  */
 package webService;
 
+import com.firebase.client.Firebase;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.jws.WebService;
@@ -51,12 +54,7 @@ private static String readUrl(String urlString) throws Exception {
     
     public static void main(String [] args)
     {
-        blogService service = new blogService();
-        try {
-            System.out.println(service.readUrl("https://resplendent-heat-7949.firebaseio.com/user.json"));
-        } catch (Exception ex) {
-            Logger.getLogger(blogService.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        
         
     }
 
@@ -64,9 +62,32 @@ private static String readUrl(String urlString) throws Exception {
      * Web service operation
      */
     @WebMethod(operationName = "addUser")
-    public Boolean addUser(@WebParam(name = "username") String username, @WebParam(name = "password") String password, @WebParam(name = "peran") String peran) {
+    public String addUser(@WebParam(name = "username") String username, @WebParam(name = "nama") String nama, @WebParam(name = "password") String password, @WebParam(name = "email") String email, @WebParam(name = "role") String role) {
         //TODO write your implementation code here:
-        
-        return null;
+        blogService service = new blogService();
+        try {
+            //System.out.println(service.readUrl("https://resplendent-heat-7949.firebaseio.com/user.json"));
+            //System.out.println(service.readUrl("https://simpleblog5.firebaseio.com/post.json"));
+            //User user = new User("usernameAkhfa", "passwordAkhfa", "namaAkhfa", "emailAkhfa", "roleAkhfa");
+            //User user2 = new User("username2Akhfa", "password2Akhfa", "nama2Akhfa", "email2Akhfa", "role2Akhfa");
+            
+            Firebase ref = new Firebase("https://if3110-4.firebaseio.com/");
+            Firebase userRef = ref.child("user");
+            Map <String, String> users = new HashMap<>();
+            users.put("username", username);
+            users.put("password", password);
+            users.put("nama", nama);
+            users.put("email", email);
+            users.put("role",role);
+            userRef.push().setValue(users);
+            
+            //ObjectMapper mapper = new ObjectMapper();
+            //User user1 = mapper.readValue(service.readUrl("https://resplendent-heat-7949.firebaseio.com/user.json"), User.class);
+        } catch (Exception ex) {
+            Logger.getLogger(blogService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "success";
     }
+
+
 }
