3f6d351535200879d3d56b5c8b0090f011c5cd1c
indmhmmd <indam.muhammad@gmail.com>
2014-12-17 14:01:34 +0700

    AddPost AddMember

--- simpleblog/src/java/webservices/ws.java
+++ simpleblog/src/java/webservices/ws.java
@@ -18,18 +18,36 @@ import javax.jws.WebParam;
  */
 @WebService(serviceName = "ws")
 public class ws {
-    private Firebase root = new Firebase("https://makan.firebaseio.com/");
 
     /**
      * This is a sample web service operation
      */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
+    @WebMethod(operationName = "AddPost")
+    public String AddPost(@WebParam(name = "Judul") String Judul,@WebParam(name = "Tanggal") String Tanggal,@WebParam(name = "Konten") String Konten,@WebParam(name = "Status") String Status,@WebParam(name = "Deleted") String Deleted) {
+        Firebase root = new Firebase("https://makan.firebaseio.com/");
         Firebase temp = root.child("post");
         Map<String,String> post = new HashMap<String,String>();
-        post.put("Judul", txt);
+        post.put("Judul", Judul);
+        post.put("Tanggal", Tanggal);
+        post.put("Status", Status);
+        post.put("Konten", Konten);
+        post.put("Deleted", Deleted);
         temp.push().setValue(post);
         
-        return "Hello " + txt + " !";
+        return "Add post success!";
+    }
+ 
+    @WebMethod(operationName = "AddMember")
+    public String AddMember(@WebParam(name = "Email") String Email,@WebParam(name = "Name") String Name,@WebParam(name = "Password") String Password,@WebParam(name = "Role") String Role) {
+        Firebase root = new Firebase("https://makan.firebaseio.com/");
+        Firebase temp = root.child("member");
+        Map<String,String> member = new HashMap<String,String>();
+        member.put("Email", Email);
+        member.put("Name", Name);
+        member.put("Password", Password);
+        member.put("Role", Role);
+        temp.push().setValue(member);
+        
+        return "Add member success!";
     }
 }
