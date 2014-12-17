60744c1f7468ec2ae17c3d59081db6fdcb2ebc0b
Mario Tressa Juzar <mariotj.tj@gmail.com>
2014-12-17 18:14:49 +0700

    add some tag

--- src/main/java/paket/simpleblog.java
+++ src/main/java/paket/simpleblog.java
@@ -7,6 +7,8 @@ package paket;
 import com.firebase.client.Firebase;
 import java.util.ArrayList;
 import java.util.Formatter;
+import java.util.HashMap;
+import java.util.Map;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -21,15 +23,16 @@ import javax.xml.ws.ResponseWrapper;
 @WebService(serviceName = "simpleblog")
 public class simpleblog {
 
-    private Firebase fbase;
+    private final Firebase fbase;
     String dbname = "https://sizzling-heat-3244.firebaseio.com/";
-        
-    public simpleblog(){
-        fbase = new Firebase(dbname);
+
+    public simpleblog() {
+        this.fbase = new Firebase(dbname);
     }
-    
+        
     /**
      * Web service operation
+     * @return 
      */
     @WebMethod(operationName = "listPost")
     public ArrayList<Post> listPost() {
@@ -39,15 +42,20 @@ public class simpleblog {
 
     /**
      * Web service operation
+     * @param id
+     * @return 
      */
     @WebMethod(operationName = "deletePost")
     public Boolean deletePost(@WebParam(name = "id") Integer id) {
-        //TODO write your implementation code here:
-        return null;
+        Firebase post = fbase.child("post").child(id.toString());
+        post.removeValue();
+        return true;
     }
 
     /**
      * Web service operation
+     * @param id
+     * @return 
      */
     @WebMethod(operationName = "publishPost")
     public Boolean publishPost(@WebParam(name = "id") Integer id) {
@@ -57,15 +65,27 @@ public class simpleblog {
 
     /**
      * Web service operation
+     * @param nama
+     * @param email
+     * @param role
+     * @param password
+     * @return 
      */
     @WebMethod(operationName = "addUser")
-    public Boolean addUser(@WebParam(name = "nama") String nama, @WebParam(name = "email") String email, @WebParam(name = "role") String role) {
-        //TODO write your implementation code here:
-        return null;
+    public Boolean addUser(@WebParam(name = "nama") String nama, @WebParam(name = "email") String email, @WebParam(name = "role") String role, @WebParam(name = "password") String password) {
+        Firebase users = fbase.child("user");
+        HashMap<String, String> newUser = new HashMap<>();
+        newUser.put("nama", nama);
+        newUser.put("email", email);
+        newUser.put("role", role);
+        newUser.put("password", password);
+        users.push().setValue(newUser);
+        return true;
     }
 
     /**
      * Web service operation
+     * @return 
      */
     @WebMethod(operationName = "listUser")
     public ArrayList<User> listUser() {
@@ -75,6 +95,11 @@ public class simpleblog {
 
     /**
      * Web service operation
+     * @param id
+     * @param nama
+     * @param role
+     * @param email
+     * @return 
      */
     @WebMethod(operationName = "editUser")
     public Boolean editUser(@WebParam(name = "id") Integer id, @WebParam(name = "nama") String nama, @WebParam(name = "role") String role, @WebParam(name = "email") String email) {
@@ -84,6 +109,8 @@ public class simpleblog {
 
     /**
      * Web service operation
+     * @param id
+     * @return 
      */
     @WebMethod(operationName = "deleteUser")
     public Boolean deleteUser(@WebParam(name = "id") Integer id) {
@@ -93,6 +120,10 @@ public class simpleblog {
 
     /**
      * Web service operation
+     * @param nama
+     * @param email
+     * @param konten
+     * @return 
      */
     @WebMethod(operationName = "addComment")
     public Boolean addComment(@WebParam(name = "nama") String nama, @WebParam(name = "email") String email, @WebParam(name = "konten") String konten) {
@@ -102,6 +133,7 @@ public class simpleblog {
 
     /**
      * Web service operation
+     * @return 
      */
     @WebMethod(operationName = "listComment")
     public ArrayList<Comment> listComment() {
@@ -111,6 +143,8 @@ public class simpleblog {
 
     /**
      * Web service operation
+     * @param id
+     * @return 
      */
     @WebMethod(operationName = "deleteComment")
     public Boolean deleteComment(@WebParam(name = "id") Integer id) {
@@ -120,6 +154,8 @@ public class simpleblog {
 
     /**
      * Web service operation
+     * @param query
+     * @return 
      */
     @WebMethod(operationName = "search")
     public ArrayList<Post> search(@WebParam(name = "query") String query) {
@@ -129,21 +165,42 @@ public class simpleblog {
 
     /**
      * Web service operation
+     * @param judul
+     * @param konten
+     * @param tanggal
+     * @return 
      */
     @WebMethod(operationName = "addPost")
-    public Boolean addPost(@WebParam(name = "judul") String judul, @WebParam(name = "konten") String konten, @WebParam(name = "tanggal") XMLGregorianCalendar tanggal) {
-        //TODO write your implementation code here:
-        return null;
+    public Boolean addPost(@WebParam(name = "judul") String judul, @WebParam(name = "konten") String konten, @WebParam(name = "tanggal") String tanggal) {
+        Firebase posts = fbase.child("post");
+        Map<String, Object> newPost = new HashMap<>();
+        newPost.put("judul", judul);
+        newPost.put("konten", konten);
+        newPost.put("tanggal", tanggal);
+        newPost.put("published", false);
+        newPost.put("deleted", false);
+        posts.push().setValue(newPost);
+        return true;
     }
 
     /**
      * Web service operation
+     * @param id
+     * @param judul
+     * @param konten
+     * @param tanggal
+     * @return 
      */
     @WebMethod(operationName = "editPost")
     @RequestWrapper(className = "paket.editPost")
     @ResponseWrapper(className = "paket.editPostResponse")
-    public Boolean editPost(@WebParam(name = "id") Integer id, @WebParam(name = "judul") String judul, @WebParam(name = "konten") String konten, @WebParam(name = "tanggal") XMLGregorianCalendar tanggal) {
-        //TODO write your implementation code here:
-        return null;
+    public Boolean editPost(@WebParam(name = "id") Integer id, @WebParam(name = "judul") String judul, @WebParam(name = "konten") String konten, @WebParam(name = "tanggal") String tanggal) {
+        Firebase post = fbase.child("post").child(id.toString());
+        Map<String, Object> newPost = new HashMap<>();
+        newPost.put("judul", judul);
+        newPost.put("konten", konten);
+        newPost.put("tanggal", tanggal);
+        post.updateChildren(newPost);
+        return true;
     }
 }
