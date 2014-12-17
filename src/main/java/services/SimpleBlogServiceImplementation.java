9e4084878a78ab789f8ec532ec1f49339293621d
azaky <a_zaky003@yahoo.com>
2014-12-17 15:11:44 +0700

    Implementasi method addPost

--- src/main/java/services/SimpleBlogServiceImplementation.java
+++ src/main/java/services/SimpleBlogServiceImplementation.java
@@ -6,7 +6,12 @@
 
 package services;
 
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 import model.Comment;
 import model.Post;
 import model.User;
@@ -16,10 +21,56 @@ import model.User;
  * @author Ahmad Zaky
  */
 public class SimpleBlogServiceImplementation implements SimpleBlogService {
-
+    /* Konstanta terkait Firebase */
+    private final String FIREBASE_URL = "https://intense-heat-3835.firebaseio.com/";
+    private final String FIREBASE_POST = "post";
+    private final String FIREBASE_USER = "user";
+    private final String FIREBASE_COMMENT = "comment";
+    private final Firebase firebaseRoot = new Firebase(FIREBASE_URL);
+    private final Firebase firebasePost = firebaseRoot.child(FIREBASE_POST);
+    private final Firebase firebaseUser = firebaseRoot.child(FIREBASE_USER);
+    private final Firebase firebaseComment = firebaseRoot.child(FIREBASE_COMMENT);
+    
+    public class TransactionResult implements Firebase.CompletionListener {
+        
+        private AtomicBoolean _done = new AtomicBoolean(false);
+        private boolean _success;
+        
+        @Override
+        public void onComplete(FirebaseError error, Firebase frbs) {
+            _done.set(true);
+            if (error != null) {
+                _success = true;
+            }
+            else {
+                _success = false;
+            }
+        }
+        
+        public boolean done() {
+            return _done.get();
+        }
+        
+        public boolean success() {
+            return _success;
+        }
+    }
+    
     @Override
     public boolean addPost(String judul, String konten, String tanggal) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // masukkan konten ke HashMap
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("judul", judul);
+        map.put("konten", konten);
+        map.put("tanggal", tanggal);
+        
+        // push ke firebase
+        TransactionResult result = new TransactionResult();
+        firebasePost.push().setValue(map, result);
+        
+        // tunggu hingga transaksi nya selesai
+        while (!result.done());
+        return result.success();
     }
 
     @Override
