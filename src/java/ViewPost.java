b0d65147df005ddabcff91ebbac962e6974e1431
henrymenori <henrymenori@yahoo.com>
2014-12-19 00:26:41 +0700

    Perbaikan error

--- src/java/ViewPost.java
+++ src/java/ViewPost.java
@@ -1,3 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
@@ -5,6 +11,10 @@ import javax.xml.ws.WebServiceRef;
 import service.ControllerImplement_Service;
 import service.Post;
 
+/**
+ *
+ * @author A 46 CB i3
+ */
 @Named(value = "viewPost")
 @SessionScoped
 public class ViewPost implements Serializable {
